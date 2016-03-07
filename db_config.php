595a06419bcc761525733dc41dd0a9cfc841e25b
Jong Hoon Lim <jhlim84@hotmail.com>
2016-03-07 17:16:42 -0600

    db_connect.php now successfully connects to the online MySQL server.

--- db_config.php
+++ db_config.php
@@ -4,8 +4,12 @@
  * All database connection variables
  */
  
-define('DB_USER', "jhlim84"); // db user
-define('DB_PASSWORD', "jonghoonlim"); // db password (mention your db password here)
-define('DB_DATABASE', "cnsoitinventory"); // database name
-define('DB_SERVER', "localhost"); // db server
+define('DB_USER', "jongwlin_jhlim84"); // db user
+define('DB_PASSWORD', "jonghoonlim1"); // db password (mention your db password here)
+define('DB_DATABASE', "jongwlin_cnsoitinventory"); // database name
+define('DB_SERVER', "server173"); // db server
+// define('DB_USER', "jhlim84"); // db user
+// define('DB_PASSWORD', "jonghoonlim"); // db password (mention your db password here)
+// define('DB_DATABASE', "cnsoitinventory"); // database name
+// define('DB_SERVER', "localhost"); // db server
 ?>
---
+++ test_connection.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "server173";
+$username = "jongwlin_jhlim84";
+$password = "jonghoonlim1";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+?>
